c2509a36b5611b7f41ad58e5da8d4a90dc8c4aa0
Tonboti <164788146+Tonboti@users.noreply.github.com>
2024-12-20 22:37:23 +0100

    ajout conversion de bases
    conversion en bases < 10 est un flop

--- 1.py
+++ 1.py
@@ -16,11 +16,15 @@ class alphabet:
 
 
 # Simple conversion d'array en string 
-def charArrayToString(array) :
+def arrayToString(array) :
     outputString = ''
     for i in range(len(array)) : 
         outputString += array[i]
     return outputString 
+
+def intArrayToString(my_list, separator = ''):
+    array = separator.join(map(str, my_list))
+    return int(array)
 
 # Encrypte l'index des lettres grace a une fonction mathematique, a changer si souhaite
 def transformY(x) :
@@ -33,7 +37,7 @@ def positionOverflow(place, nbMax = 26) :
     return b 
 
 # Cree un alphabet encrypte - donc la clef selon la fonction transformY
-def createKey(referenceAlphabet = alphabet.array ):
+def createKey(returnIndex = False, referenceAlphabet = alphabet.array):
     # clef 
     alphabet = []
     # array qui recueille les nombres d'index deja utilises pour pallier a une reutilisation de lettres pour deux lettres differentes 
@@ -55,7 +59,11 @@ def createKey(referenceAlphabet = alphabet.array ):
         # ajoute l'index determine ci-dessus a l'array des index utilises
         arrayUsedIndex.append(newIndex)
     
-    return alphabet
+    if returnIndex == False :
+        return alphabet
+    else :
+        arrayUsedIndex = ["{:02}".format(num) for num in arrayUsedIndex]
+        return arrayUsedIndex
 
 
 #########################################################################
@@ -90,14 +98,80 @@ def areTwoLettersTheSame(array) :
         seen.add(num)
         
 
+########################## Raccourcir la clef ############################
+
+# Permet de changer de base 10 a base x l'input
+def fromBase10(number, base) : 
+    resteArray  = []
+    quotient    = number
+    reste       = 1
+
+    # Conversion de base 10 a base x
+    while reste != 0 :
+        reste   = quotient % base
+        quotient= quotient // base
+        resteArray.append(reste)
+
+    resteArray.reverse()
+    
+    # Transforme les valeurs superieures a 10 en lettres
+    letters = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    for i in range(len(resteArray)) :
+        bufferReste = resteArray[i]
+        if bufferReste >= 10 :
+            bufferReste = letters[bufferReste-10]
+        resteArray[i] = bufferReste  
+
+    # Il y a souvent un 0 inutile en debut, donc autant l'enlever
+    if resteArray[0] == 0 : 
+        resteArray.pop(0) 
+
+    return resteArray
+
+# Nombre en base x en base 10
+def toBase10(numberStr, base = 16) :
+    intArray = []
+    array = list(str(numberStr))
+    array.reverse()
+    output = 0
+
+    # conversion de char en nombre si base superieure a 10
+    if base >=10 :
+        letters = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        for i in range(len(array)):
+            if array[i] in letters :
+                array[i] = letters.index(array[i]) + 10
+            intArray.append(int(array[i]))
+
+    # sinon conversion du char array en int array 
+    else :
+        for char in array:
+            intArray.append(int(char))
+    
+    # conversion en base 10
+    for i in range(len(intArray)) :
+        output += intArray[i]*(base** i)
+    return output
+
+
+def fromBaseToBase(number, startBase, finalBase) :
+    a = toBase10(number, startBase)
+    return fromBase10(a, finalBase)
+
+print(fromBaseToBase('C3B', 16, 2))
 #########################################################################
 #########################################################################
 
+# print(fromBase10(intArrayToString(createKey(True)),32))
 
-sth = encrypt(file.fileContent)
-print('\n')
-print( 'Key       : ', createKey())
-print( 'Encrypted : ', charArrayToString(sth))
-print( 'Decrypted : ', charArrayToString(decrypt(sth)))
-print('\n')
+# print(
+#     toBase(
+#         intArrayToString(createKey(True))
+#         ), 16)
+# sth = encrypt(file.fileContent)
+# print('\n')
+# print( 'Key       : ', createKey())
+# print( 'Encrypted : ', arrayToString(sth))
+# print( 'Decrypted : ', arrayToString(decrypt(sth)))
+# print('\n')
 
